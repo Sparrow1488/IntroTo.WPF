0b6f5ae1824c0d43081989e9d831bca6967ddc16
Sparrow1488 <ilyaokubev@gmail.com>
2023-01-03 01:53:30 +0300

    feat: reactiveui хуета какая то

---
+++ src/IntroTo.Reactive.Console/CanExecuteCommandLesson.cs
@@ -0,0 +1,32 @@
+﻿using System.Reactive;
+using System.Security.AccessControl;
+using System.Windows.Input;
+using ReactiveUI;
+
+namespace IntroTo.Reactive.Console;
+
+public class CanExecuteCommandLesson
+{
+    public CanExecuteCommandLesson()
+    {
+        var canSave = this.WhenAnyValue(
+            x => x.Username,
+            x => x.Password,
+            (username, password) => 
+                !string.IsNullOrWhiteSpace(username) &&
+                !string.IsNullOrWhiteSpace(password));
+
+        SaveCommand = ReactiveCommand.CreateFromTask(SaveAsync, canSave);
+    }
+    
+    public string Username { get; set; }
+    public string Password { get; set; }
+
+    public ICommand SaveCommand { get; }
+
+    private static Task SaveAsync()
+    {
+        System.Console.WriteLine("Saved");
+        return Task.CompletedTask;
+    }
+}
--- src/IntroTo.Reactive.Console/Program.cs
+++ src/IntroTo.Reactive.Console/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reactive;
 using System.Reactive.Linq;
+using IntroTo.Reactive.Console;
 using ReactiveUI;
 
 #region Observable command
@@ -21,9 +22,42 @@ using ReactiveUI;
 
 #region Synchronous command
 
-var command = ReactiveCommand.Create(() =>
-    Console.WriteLine("Reactive command"));
+// var command = ReactiveCommand.Create(() =>
+//     Console.WriteLine("Reactive command"));
+//
+// command.Execute().Subscribe();
 
-command.Execute().Subscribe();
+#endregion
+
+#region Asynchronous command
+
+// ObservableAsPropertyHelper<string[]> _names;
+// ReactiveCommand<Unit, string[]> LoadNames;
+// string[] Names => _names.Value;
+//
+// LoadNames = ReactiveCommand.CreateFromTask(LoadNamesAsync);
+//
+// _names = LoadNames.ToProperty(this, x => Names, RxApp.MainThreadScheduler);
+//
+// Task<string[]> LoadNamesAsync()
+// {
+//     Task.Delay(1000).Wait();
+//     return Task.FromResult(new[] {
+//         "Valentin",
+//         "Yuri",
+//         "Gosha"
+//     });
+// }
+    
+#endregion
+
+#region Can execute command
+
+var lesson = new CanExecuteCommandLesson();
+Console.WriteLine(lesson.SaveCommand.CanExecute(null));
+lesson.Username = "Test";
+lesson.Password = "1234";
+Console.WriteLine(lesson.SaveCommand.CanExecute(null));
+if(lesson.SaveCommand.CanExecute(null)) lesson.SaveCommand.Execute(null);
 
 #endregion
